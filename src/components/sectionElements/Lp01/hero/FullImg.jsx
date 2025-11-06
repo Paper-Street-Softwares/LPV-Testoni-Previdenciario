f909cc87e42690b2e3a1aa95b8d60d9b08b430cc
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-06 09:51:40 -0300

    fix: imagem Hero

--- src/components/sectionElements/Lp01/hero/FullImg.jsx
+++ src/components/sectionElements/Lp01/hero/FullImg.jsx
@@ -22,7 +22,7 @@ function FullImg({ obs }) {
           }}
         >
           {/* Gradiente por cima da imagem */}
-          <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-lpvHeroGradientBot z-20"></div>
+          <div className="absolute inset-0 bg-gradient-to-b from-black/0 to-lpvHeroGradientBot z-20"></div>
 
           {/* Conteúdo acima de tudo */}
           <div className="relative z-30">
